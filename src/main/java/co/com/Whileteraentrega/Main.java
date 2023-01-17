c3f97592fefcee690c5133da774849bcb011638e
Ervingarcia13 <ervingarcia13@hotmail.com>
2023-01-17 05:12:49 -0500

    Haciendo una  prueba

---
+++ src/main/java/co/com/Whileteraentrega/Main.java
@@ -0,0 +1,19 @@
+package co.com.Whileteraentrega;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int  Numerowhile=-10;
+
+        while (Numerowhile <=3){
+
+            Numerowhile =Numerowhile +1;
+
+            System.out.println("El valor es "+Numerowhile  );
+
+        }
+
+        System.out.println("No cumple  la condicion "  );
+
+ }
+}
---
+++ src/main/java/co/com/Whileteraentrega/Whileteraentrega.java
@@ -0,0 +1,6 @@
+package co.com.Whileteraentrega;
+
+public class Whileteraentrega {
+
+
+}
